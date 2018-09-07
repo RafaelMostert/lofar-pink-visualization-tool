4692ad90e0e48e26d02687d45b44b279e4d8511f
Rafael Mostert <mostert@strw.leidenuniv.nl>
2018-09-07 12:03:34 +0200

    updated css inserted bootstrap copyright notice

--- website_files/outliers_ID449.php
+++ website_files/outliers_ID449.php
@@ -140,7 +140,7 @@ width:440px;
 	<meta name="robots" content="noindex,nofollow,noarchive,nosnippet,noodp">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <script src="http://code.jquery.com/jquery-1.10.0.min.js"></script>
+    <script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
     <!-- Include ASTRON favicon -->
     <link rel="icon" href="images/favicon.ico">
     <!-- Popover CSS 
@@ -414,3 +414,29 @@ foreach($outliers_subdirnames  as $outliers_subdirname) {
 </body>
 
 </html>
+
+
+<!-- Copyright notification related to the use of Bootstrap code snippets:
+
+The MIT License (MIT)
+
+Copyright (c) 2011-2016 Twitter, Inc.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+-->
